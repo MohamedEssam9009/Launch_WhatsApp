8141412cfbc5668fbad0f811751b3e51e63bbb6f
Mohamed Essam <77198018+MohamedEssam9009@users.noreply.github.com>
2024-04-15 22:52:52 +0200

    finish the app

---
+++ lib/home_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [Colors.blue, Colors.green],
+          ),
+        ),
+        child: Center(
+          child: InkWell(
+            onTap: () async {
+              var whatsAppNumber = '+201019664820';
+              var whatsAppUrl =
+                  'whatsapp://send?phone=$whatsAppNumber&text=Hello Mohamed';
+              await launchUrl(Uri.parse(whatsAppUrl));
+            },
+            child: Container(
+              height: 50.0,
+              width: 160.0,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: const Center(
+                child: Text(
+                  'Open WhatsApp',
+                  style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
